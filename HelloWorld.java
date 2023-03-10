2ca83fee48c25d885cea48cc01e94b711e04cf63
Rahulsagar123 <rahulsagarazure@gmail.com>
2023-03-10 16:57:08 +0530

    new files are added

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class HelloWorld
+{
+public static void main(String[] args)
+	{
+	int count=0;
+while(true)
+{
+System.out.println("Hello world:");
+count++;
+}
+if (count==3)
+	{
+	System.out.println("U have luimited time");
+	
+	}
+
+}
+}
+
+
